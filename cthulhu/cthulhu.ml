35a50e434bad7f264cf6e51cb3948112a607d603
Yul3n <aghilas.boussaa@laposte.net>
2020-03-26 12:08:21 +0100

    Finished the parser.

--- cthulhu/cthulhu.ml
+++ cthulhu/cthulhu.ml
@@ -1,3 +1,5 @@
+exception Invalid_instruction
+
 type instruction =
   | Incr
   | Decr 
@@ -8,17 +10,35 @@ type instruction =
   | SwapE of int 
   | Swape of int 
       
-let eval input funs = 
-  let strings = String.sep ' ' input in 
-  let rec parse_int input pos start =
-    match String.get input pos with
-    | '0' | '1' | | '3' | '4' | '5' | '6' | '7' | 
-      '8' | '9' | 'A' | 'B' | 'C' | 'D' ->
-        minint input (pos + 1) start
-    | _   -> int_of_string "0x" ^ (String.sub start (pos - 1))
+let eval input =
+  let strings = String.split_on_char ' ' input in
+      let rec minint input pos start =
+        match String.get input pos with
+        | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' |  '8' | '9' | 'A' | 'B'
+        | 'C' | 'D' -> minint input (pos + 1) start
+        | _   -> [int_of_string ("0x" ^ (String.sub input start (pos - 1)));
+                  pos - start]
   in
   let rec parse input pos =
     match pos with 
       _ when pos = String.length input -> []
-    | _ -> match String.get input pos with
-        'i'
+    | _ ->
+      begin
+        match (String.get input pos) with
+        | 'i' -> Incr :: parse input (pos + 1)
+        | 'd' -> Decr :: parse input (pos + 1)
+        | '[' -> let arg = minint input (pos + 1) (pos + 1) in
+          Call (List.hd arg) :: parse input (pos + List.nth arg 2)
+        | ']' -> let arg = minint input (pos + 1) (pos + 1) in
+          PartCall (List.hd arg) :: parse input (pos + List.nth arg 2)
+        | 'E' -> let arg = minint input (pos + 1) (pos + 1) in
+          SwapE (List.hd arg) :: parse input (pos + List.nth arg 2)
+        | 'e' -> let arg = minint input (pos + 1) (pos + 1) in
+          Swape (List.hd arg) :: parse input (pos + List.nth arg 2)
+        | 'o' -> Out  :: parse input (pos + 1)
+        | '*' -> In   :: parse input (pos + 1)
+        | _   -> raise Invalid_instruction
+      end
+  in
+  let instrs = (int_of_string ("0x" ^ (List.hd strings)), parse(List.nth strings 2) 0) in
+  let rec exec
