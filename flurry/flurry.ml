8aea520d7507fcf5a96abce6d12cb949574b46e0
Yul3n <aghilas.boussaa@laposte.net>
2020-03-29 14:11:08 +0000

    FLURRY - Started the lexer.

--- flurry/flurry.ml
+++ flurry/flurry.ml
@@ -1,34 +1,3 @@
-
-Bienvenue sur OCaml 4.10.0
- - entrez une phrase OCaml dans le champ ci-dessous et appuyez sur [Entrée]
- - utilisez [Maj-entrée] pour passer à la ligne sans déclencher d'exécution
- - utilisez [Ctrl-↑] pour retrouver votre entrée précédente
- - utilisez [Ctrl-↑] / [Ctrl-↓] pour naviguer dans l'historique
-Le toplevel a été redémarré.
-type combinator = | S | K | I
-type token = | Nil_parent 
-             | Nil_curl 
-             | Nil_brack 
-             | Nil_angl 
-             | Mon_parent of token list
-             | Mon_curl   of token list
-             | Mon_brack  of token list
-             | Mon_angl   of token list
-let eval input stack =
-  let rec lex input pos =
-    match pos with
-      len when len > ((String.length input) - 1) -> []
-    | _ -> 
-        match (String.get input pos), (String.get input (pos + 1) with
-          '<', '>' -> Nil_angl :: lex input (pos + 2)
-        | '(', ')' ->
-          
-  in ;;
-Line 15, characters 66-70:
-Error: Syntax error: ')' expected
-Line 15, characters 38-39:
-  This '(' might be unmatched
-Le toplevel a été redémarré.
 type combinator = | S | K | I
 type token = | Nil_paren 
              | Nil_curly
@@ -39,14 +8,22 @@ type token = | Nil_paren
              | Mon_brack of token list
              | Mon_angle of token list
 let eval input stack =
-  let rec lex input pos =
+  let rec lex pos =
+    let rec goto num chr alt_chr pos =
+      match String.get input pos with
+        c when alt_chr = c -> goto (num + 1) chr alt_chr (pos + 1)
+      | c when chr = c && num = 0 -> pos + 1
+      | c when chr = c -> goto (num -1) chr alt_chr (pos + 1)
+      | _ -> goto num chr alt_chr (pos + 1)
+    in
     match pos with
       len when len > ((String.length input) - 1) -> []
     | _ -> 
         match (String.get input pos), (String.get input (pos + 1)) with
-          '<', '>' -> Nil_angle :: lex input (pos + 2)
-        | '(', ')' -> Nil_paren :: lex input (pos + 2)
-        | '[', ']' -> Nil_brack :: lex input (pos + 2)
-        | '{', '}' -> Nil_curly :: lex input (pos + 2)
-        | 
+          '<', '>' -> Nil_angle :: lex (pos + 2)
+        | '(', ')' -> Nil_paren :: lex (pos + 2)
+        | '[', ']' -> Nil_brack :: lex (pos + 2)
+        | '{', '}' -> Nil_curly :: lex (pos + 2)
+        | '(', _   -> Mon_paren (lex (pos + 1)) ::
+                      lex (goto 0 ')' '(' (pos + 1))
   in
