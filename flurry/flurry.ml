04d92db3416fbfe2d0e454a7d8c0cd7e4ba0a66d
Yul3n <39010883+Yul3n@users.noreply.github.com>
2020-03-30 11:49:34 +0200

    FLURRY - Finished the lexer.

--- flurry/flurry.ml
+++ flurry/flurry.ml
@@ -7,13 +7,14 @@ type token = | Nil_paren
              | Mon_curly of token list
              | Mon_brack of token list
              | Mon_angle of token list
+                   
 let eval input stack =
   let rec lex pos =
     let rec goto num chr alt_chr pos =
       match String.get input pos with
         c when alt_chr = c -> goto (num + 1) chr alt_chr (pos + 1)
       | c when chr = c && num = 0 -> pos + 1
-      | c when chr = c -> goto (num -1) chr alt_chr (pos + 1)
+      | c when chr = c -> goto (num - 1) chr alt_chr (pos + 1)
       | _ -> goto num chr alt_chr (pos + 1)
     in
     match pos with
@@ -26,4 +27,11 @@ let eval input stack =
         | '{', '}' -> Nil_curly :: lex (pos + 2)
         | '(', _   -> Mon_paren (lex (pos + 1)) ::
                       lex (goto 0 ')' '(' (pos + 1))
+        | '{', _   -> Mon_curly (lex (pos + 1)) ::
+                      lex (goto 0 '}' '{' (pos + 1))
+        | '[', _   -> Mon_brack (lex (pos + 1)) ::
+                      lex (goto 0 ']' '[' (pos + 1))
+        | '<', _   -> Mon_angle (lex (pos + 1)) ::
+                      lex (goto 0 '>' '<' (pos + 1))
+        | _, _     -> lex (pos + 2)
   in
