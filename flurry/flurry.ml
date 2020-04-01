4d1f4f0512932356cced0e6be33d246c6667468b
Yul3n <aghilas.boussaa@laposte.net>
2020-04-01 11:42:02 +0200

    FLURRY - Started with the new specification.

--- flurry/flurry.ml
+++ flurry/flurry.ml
@@ -13,7 +13,7 @@ type combinator =
   | K
   | S
   | T of combinator * combinator
-         
+        
 let rec church =
   function
     0 -> T(K, I)
@@ -46,22 +46,55 @@ let eval input stack =
         | '<', _   -> Mon_angle (lex (pos + 1)) ::
                       lex (goto 0 '>' '<' (pos + 1))
         | _, _     -> lex (pos + 2)
-  in 
+  in
+  (* This part is -stolen- inspired by : https://github.com/ngzhian/ski *)
+  let rec reduce tokens combs =
+    let rec to_comb comb toks =
+      match toks with
+        [] -> comb
+      | hd :: tl ->
+        to_comb (T(comb, hd)) tl
+    in
+    let final_comb = to_comb (List.hd tokens)  (List.tl tokens)
+    in
+    let rec simplify comb =
+      match comb with
+        I | K | S              -> comb
+      | T (I, x)               -> simplify x
+      | T (T (K, x), _)        -> simplify x
+      | T (T (T (S, K), K), x) -> simplify x
+      | T (T (T (S, I), I), x) -> simplify (T(x, x))
+      | T (T (T (S, x), y), z) ->
+        simplify (T ((T (x, z)), T(y, z)))
+      | T (left, right)        ->
+        let left' = simplify left in
+        let right' = simplify right in
+        if left = left' && right = right'
+        then T (left, right)
+        else simplify (T (left', right'))
+    in
+  in
   let rec exec tokens stack =
     match tokens with 
       [] -> [], stack
-    | Nil_angle :: tl -> T(S,K), stack, tl
-    | Nil_paren :: tl -> K, stack, tl
+    | Nil_angle :: tl ->
+      let combtl, nstack = exec tl stack in
+      S :: combtl, nstack
+    | Nil_paren :: tl ->
+      let combtl, nstack = exec tl stack in
+      K :: combtl, nstack
     | Nil_brack :: tl -> (church (List.length stack)), stack, tl
     | Nil_curly :: tl -> 
         begin 
           match stack with
-            [] -> I, []
+            [] ->
+            let combtl, nstack = exec tl stack in
+            I :: combtl, nstack
           | hd :: tl -> hd, tl
-        end, tl
+        end
     | Mon_paren (body) :: tl ->
         let n, nstack = exec body stack in
-        n, n :: nstack, tl
+        n, n :: nstack
     | Mon_brack (body) :: tl ->
         exec body stack
     | Mon_curly (body) :: tl -> 
